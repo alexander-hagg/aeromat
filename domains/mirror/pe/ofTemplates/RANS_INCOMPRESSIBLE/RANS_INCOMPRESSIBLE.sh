d990764f9293716f84bc44666625ec373a663ab3
Alexander Hagg <info@haggdesign.de>
2019-10-08 11:42:28 +0200

    Added new templates for RANS incompressible and LES compressible

---
+++ domains/mirror/pe/ofTemplates/RANS_INCOMPRESSIBLE/RANS_INCOMPRESSIBLE.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --partition=any
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --mem=120G
+#SBATCH --output=slurm.%j.out   
+#SBATCH --error=slurm.%j.err    
+
+module load openmpi/gnu
+module load mpitools/default
+module load gcc/default
+
+. ~/OpenFOAM/OpenFOAM-v1906/etc/bashrc
+
+echo "Number of tasks: " echo $SLURM_NTASKS
+
+## Clean
+. $WM_PROJECT_DIR/bin/tools/CleanFunctions
+cleanCase
+
+start=`date +%s`
+
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+### generate mesh
+runApplication blockMesh
+runApplication surfaceFeatureExtract
+runApplication decomposePar
+
+mpirun -np 32 snappyHexMesh  -parallel  -overwrite
+
+meshTime=`date +%s`
+echo "$((meshTime-start))" >> mesh.timing
+
+# reconstructParMesh -constant
+# rm -r proc*
+# rm log.*
+
+## run RANS  #restore0Dir -processor
+runApplication decomposePar
+
+## Copy initial conditions
+ls -d processor* | xargs -I {} rm -rf ./{}/0
+ls -d processor* | xargs -I {} cp -r 0 ./{}/0
+
+mpirun -np 32 patchSummary
+mpirun -np 32 simpleFoam -parallel
+
+cfdEnd=`date +%s`
+echo "$((cfdEnd-cfdStart))" >> cfd.timing
+
+
+runApplication reconstructPar -latestTime
+##rm -r processor*
+
+end=`date +%s`
+echo "$((end-start))" >> all.timing
+
+# Return results
+cp postProcessing/forceCoeffs1/0/forceCoeffs.dat result.dat
+cp postProcessing/mirror/0/forces.dat forces.dat
---
+++ domains/mirror/pe/ofTemplates/RANS_INCOMPRESSIBLE/caseRunner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+startFile="start.signal"
+doneFile="done.signal"
+stopFile="stop.signal"
+
+./Allclean
+rm _inactive.state
+touch _active.state
+
+while true
+sleep 1s
+do
+
+if [ -f "$stopFile" ]
+then
+	echo "$stopFile found, ending caseRunner."
+	rm _active.state
+	touch _inactive.state
+	break;
+fi
+
+if [ -f "$startFile" ]
+	then
+	echo "$startFile found: starting OpenFOAM case."
+	bash RANS_INCOMPRESSIBLE.sh
+else
+	echo -n "Waiting for $startFile..."
+fi
+
+done
