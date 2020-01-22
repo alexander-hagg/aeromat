6df3828e7aaf34fb5b04b1dc27b841dc42213cfb
Alexander Hagg <info@haggdesign.de>
2020-01-22 12:26:06 +0100

    Reconstruct multiple cases and create tarball with script

---
+++ reconstructCases.sh
@@ -0,0 +1,11 @@
+for dir in case*
+do 
+	if [ -d "$dir" ]; then
+		cd "$dir";
+		echo "$dir";
+		reconstructParMesh -constant;
+		# reconstructPar;
+		cd "..";
+		tar -cvzf "$dir".tar "$dir"
+	fi
+done
